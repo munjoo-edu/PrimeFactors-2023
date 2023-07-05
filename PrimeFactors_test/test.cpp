914f6d48ae1b5647ab29727fff946da1535d478b
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-05 15:30:28 +0900

    [GREEN] Pass Of2 test
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- PrimeFactors_test/test.cpp
+++ PrimeFactors_test/test.cpp
@@ -9,3 +9,9 @@ TEST(TestPrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(TestPrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
