15705009d88ef33c6997e0e51452fd58d4b1e4d1
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-05 15:59:05 +0900

    [GREEN] Pass Of12 test
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -7,7 +7,7 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
-			if (number == 4 || number == 6 || number == 9) {
+			if (number == 4 || number == 6 || number == 9 || number == 12) {
 				for (int divisor = 2; number > 1; divisor++) {
 					while (number % divisor == 0) {
 						result.push_back(divisor);
