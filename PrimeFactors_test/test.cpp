e72e7a7da0da568f928167097bf1c6d9ba656651
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-05 15:46:41 +0900

    [GREEN] Pass Of6 test
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- PrimeFactors_test/test.cpp
+++ PrimeFactors_test/test.cpp
@@ -29,3 +29,8 @@ TEST_F(TestPrimeFactorsFixture, Of4) {
 	expected = { 2, 2 };
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST_F(TestPrimeFactorsFixture, Of6) {
+	expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
